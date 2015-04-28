ba2f4a7a79f11c7d020975dbe2d467301b7e581f
Vincent Jacques <vincent@vincent-jacques.net>
2015-04-28 09:32:17 +0200

    Let's go!

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf8
+
+# Copyright 2015 Vincent Jacques <vincent@vincent-jacques.net>
+
+import setuptools
+
+version = "0.0.0"
+
+
+setuptools.setup(
+    name="variadic",
+    version=version,
+)
---
+++ variadic.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+# Copyright 2015 Vincent Jacques <vincent@vincent-jacques.net>
+
+import unittest
+
+
+if __name__ == "__main__":
+    unittest.main()
