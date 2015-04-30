00f83e2e8dd0757b3262f5f5bbe40be9525013ad
Vincent Jacques <vincent@vincent-jacques.net>
2015-04-30 19:22:38 +0200

    Standardize doc/conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -4,26 +4,29 @@
 
 master_doc = "index"
 project = "variadic"
-copyright = "2015, Vincent Jacques"
-author = "Vincent Jacques"
+author = '<a href="http://vincent-jacques.net/contact">Vincent Jacques</a>'
+copyright = "{} 2015".format(author)
 extensions = []
+
+
 nitpicky = True
+# nitpick_ignore
+
 
 # https://github.com/bitprophet/alabaster
 # html_theme_path
 extensions.append("alabaster")
 html_theme = "alabaster"
 html_sidebars = {
-    "**": [
-        "about.html", "searchbox.html",
-    ]
+    "**": ["about.html", "searchbox.html"],
 }
 html_theme_options = {
     "github_user": "jacquev6",
-    "github_repo": "variadic",
+    "github_repo": project,
     "github_banner": True,
     "travis_button": True,
 }
+
 
 # http://sphinx-doc.org/ext/autodoc.html
 extensions.append("sphinx.ext.autodoc")
